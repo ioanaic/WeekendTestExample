96f75f4f5cdbf8a12c2e35d9484cca244e335826
rekabarra <rekabarra@gmail.com>
2017-02-25 10:53:20 +0200

    first java program to add 2 numbers

---
+++ src/Calc.java
@@ -0,0 +1,8 @@
+/**
+ * Created by blinstall on 2/25/2017.
+ */
+public class Calc {
+            public static int add(int a, int b){
+            return a+b;
+        }
+    }
---
+++ src/Example.java
@@ -0,0 +1,14 @@
+/**
+ * Created by blinstall on 2/25/2017.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int sum= Calc.add(2,3);
+        System.out.println(sum); //5
+    }
+
+}
+
+
