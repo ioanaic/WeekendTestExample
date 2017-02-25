652a4e559b9a4f98144bff8c15c5664f0700febe
rekabarra <rekabarra@gmail.com>
2017-02-25 10:55:51 +0200

    changed hello to good bye

--- src/Example.java
+++ src/Example.java
@@ -3,7 +3,7 @@
  */
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Good bye");
 
         int sum= Calc.add(2,3);
         System.out.println(sum); //5
